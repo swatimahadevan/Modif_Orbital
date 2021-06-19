2ecb229e4ca8b763732fd65c56b8a030a91f6145
Your Name <you@example.com>
2021-06-19 22:50:21 +0800

    revamped and brushed up UI

---
+++ connect.php
@@ -0,0 +1,15 @@
+<?php
+$server="remotemysql.com";
+$user="pBc1y4gdCU";
+$dbname="pBc1y4gdCU";
+$password="enO5z5EJpi";
+$con=mysqli_connect($server,$user,$password,$dbname);
+if($con)
+{
+	  // echo"connected";
+}
+else
+{
+	die(mysqli_connect_error());
+}  
+?>  
